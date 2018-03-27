69cb0247c07ce92dc50602608781f730ee1b6bd4
Grzegorz Chroń <grzegorz.chron@gmail.com>
2018-03-27 22:50:30 +0200

    homework - date and time

---
+++ DateAndTime/index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        $today = getdate();
+        $mth = $today['month'];
+        $weekDay = $today['weekday'];
+        $monthDay = $today['mday'];
+        $year = $today['year'];
+
+        $days = Array(
+            "Monday" => "Poniedziałek",
+            "Tuesday" => "Wtorek",
+            "Wednesday" => "Środa",
+            "Thursday" => "Czwartek",
+            "Friday" => "Piątek",
+            "Saturday" => "Sobota",
+            "Sunday" => "Niedziela",
+        );
+        $months = Array(
+            "January" => "Styczeń",
+            "February" => "Luty",
+            "March" => "Marzec",
+            "April" => "Kwiecień",
+            "May" => "Maj",
+            "June" => "Czerwiec",
+            "July" => "Lipiec",
+            "August" => "Sierpień",
+            "September" => "Wrzesień",
+            "October" => "Pańdziernik",
+            "November" => "Listopad",
+            "December" => "Grudzień",
+        );
+
+        print_r($today);
+        echo '<br/><br/>';
+        //Zadadanie 1 - Napisz skrypt, który na podstawie danych pobranych z tablicy zwróconej przez funkcję getdate() wyświetli bieżącą datę. W dacie nazwa miesiąca zostanie podana w j. pl.
+
+        echo 'dziś jest ' . $monthDay . ' ' . $months[$mth] . ' ' . $year;
+        echo '<br/><br/>';
+
+        // Zadanie 2 - Napisz skrypt któryt będzie wyświetlał bieżacy dzień tygodnia w podanej postaci: Dziś jest środa.
+
+        echo 'dziś jest ' . $days[$weekDay];
+        echo '<br/><br/>';
+        //Zadanie 3 - Napisz skrypt wyświetlający liczbę dni, które uplyneły od początku roku oraz liczbę dni, które pozostały do końca roku.
+
+        $daysFromTheBeginning = $today['yday'];
+        echo 'od początku roku mineło ' . $daysFromTheBeginning . ' dni';
+        echo '<br/><br/>';
+        $daysIn2018 = 365;
+        $daysToTheEnd = $daysIn2018 - $today['yday'];
+        echo 'do końca roku pozostało ' . $daysToTheEnd . ' dni';
+        echo '<br/><br/>';
+        
+        //Zadanie 4 - napisz skrypt, który na podstawie danych: data urodzenia 1966-09-24 obliczny ile użytkownik ma lat
+
+        
+        ?>
+    </body>
+</html>
